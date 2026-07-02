3c01e117f425633d6650dabfe3ffd4cf83cf8b7d
Arrobo, Gabriel <gabriel.arrobo@intel.com>
2026-07-01 20:38:58 -0700

    Address Copilot's comment
    Signed-off-by: Arrobo, Gabriel <gabriel.arrobo@intel.com>

--- deps/gnbsim/roles/router/templates/gnbsim-routes.sh
+++ deps/gnbsim/roles/router/templates/gnbsim-routes.sh
@@ -6,10 +6,14 @@ set -eu
 
 action="${1:-start}"
 
+{% set peer_gnbsim_nodes = [] %}
+{% if inventory_hostname in groups['master_nodes'] %}
+{% set peer_gnbsim_nodes = play_hosts | intersect(groups['gnbsim_nodes']) | difference(groups['master_nodes']) | list %}
+{% endif %}
+
 case "$action" in
   start)
 {% if inventory_hostname in groups['master_nodes'] %}
-{% set peer_gnbsim_nodes = play_hosts | intersect(groups['gnbsim_nodes']) | difference(groups['master_nodes']) %}
 {% if peer_gnbsim_nodes | length > 0 %}
 {% for item in peer_gnbsim_nodes %}
     /usr/sbin/ip route replace {{ gnbsim.router.macvlan.subnet_prefix }}.{{ lookup('ansible.utils.index_of', groups['gnbsim_nodes'], 'eq', item) }}.0/24 via {{ hostvars[item]['ansible_default_ipv4']['address'] }}
